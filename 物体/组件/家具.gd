2dd838152883f64ec8867be00a133e3140e8e873
ttwings <ttwings@qq.com>
2021-04-12 00:12:46 +0800

    掌门路地图参考

--- 物体/组件/家具.gd
+++ 物体/组件/家具.gd
@@ -2,6 +2,15 @@ extends Node
 
 onready var 持有者 = get_parent()
 
+export(int,-100,100) var 美观度 = 0
+export(int,-100,100) var 灵气 = 0 
+export(int,0,100) var 金 = 0
+export(int,0,100) var 水 = 0
+export(int,0,100) var 木 = 0
+export(int,0,100) var 火 = 0
+export(int,0,100) var 土 = 0
+export(String) var 材质 = "灵木" 
+
 var 类别
 
 func do_搬动():
